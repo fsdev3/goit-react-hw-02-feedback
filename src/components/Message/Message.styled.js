24267632d5f61d016f62d07c5b859066f8f191e0
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-07-25 18:50:22 +0200

    upd

--- src/components/Message/Message.styled.js
+++ src/components/Message/Message.styled.js
@@ -3,6 +3,6 @@ import { styled } from 'styled-components';
 export const Message = styled.div`
   margin-top: 10px;
   p {
-    font-size: 20px;
+    font-size: 18px;
   }
 `;
--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const StatList = styled.ul`
   display: flex;
   flex-direction: column;
-  gap: 4px;
+  gap: 6px;
   margin-top: 10px;
+  margin-left: 16px;
 `;
